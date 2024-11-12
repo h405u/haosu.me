a066cdf9abf3460b9ad3df246ec29f30f188e071
Hao Su <hs@imsuh.com>
2024-11-12 16:46:02 +0800

    new init

---
+++ slides/24-10-22-metaprogram/assets/slidev/default-BJneYXF4.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-IZsJrVaC.js";import{_ as r}from"../index-CLI9pqXi.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
